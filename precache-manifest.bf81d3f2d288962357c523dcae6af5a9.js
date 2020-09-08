5aa391be966b8fb3c72019cb46872090fd207dd2
giang <giangd@rocketmail.com>
2020-09-07 22:07:09 -0700

    Updates

---
+++ precache-manifest.bf81d3f2d288962357c523dcae6af5a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e19baa93cc4e8742a0a47e885cc54b",
+    "url": "/daily-dose-of-cuteness/index.html"
+  },
+  {
+    "revision": "226fc017d4df09525436",
+    "url": "/daily-dose-of-cuteness/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "226fc017d4df09525436",
+    "url": "/daily-dose-of-cuteness/static/js/2.33ae4154.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/daily-dose-of-cuteness/static/js/2.33ae4154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3ab7354f631e3c5c0e",
+    "url": "/daily-dose-of-cuteness/static/js/main.c7e2c909.chunk.js"
+  },
+  {
+    "revision": "35ed3658994ab20fbfbe",
+    "url": "/daily-dose-of-cuteness/static/js/runtime-main.8d35b0d9.js"
+  }
+]);
